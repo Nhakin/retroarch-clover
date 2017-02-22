a3b9b4fdb933176524ce5650193dad51ce97f313
Nhakin <KahnAbyss1@gmail.com>
2017-02-22 12:20:09 -0500

    Added make_release.bat

---
+++ make_release.bat
@@ -0,0 +1,24 @@
+@echo off
+Cls
+
+set releasedir=release
+
+if EXIST "%releasedir%" rd /s/q %releasedir%
+
+md release\cores
+
+set path=%path%;C:\Program Files (x86)\7-Zip
+
+zip -rq release/libretro_core_template.zip libretro_core_template.hmod/*
+zip -rq release/CloverApp.zip CloverApp
+
+cd retroarch.hmod 
+7z a -ttar * -so -bd | 7z a -tgzip -si -bd "..\release\retroarch.hmod" > nul
+cd../core_modules
+
+for /d %%d in (*.hmod) do (
+  cd %%d
+  7z a -ttar -bd * -so | 7z a -tgzip -si -bd ..\..\release\cores\%%d > nul
+  cd..
+)
+cd..
--- makehmod.bat
+++ makehmod.bat
@@ -2,12 +2,15 @@
 set moddir=retroarch.hmod
 set modname=retroarch.hmod
 set outdir=output
+set path=%path%;C:\Program Files (x86)\7-Zip
 for %%x in (%*) do set outdir=%1
+echo "%outdir%"
 
 cd %moddir%
 if not ERRORLEVEL 0 goto error
 if EXIST "%modname%" del /F /Q "%modname%"
-tar -czvf "%modname%" *
+rem tar -czvf "%modname%" *
+7z -ttar a * -so | 7z -si a -tgzip "%modname%"
 if ERRORLEVEL 9009 goto notar
 if NOT ERRORLEVEL 0 goto error
 cd ..
